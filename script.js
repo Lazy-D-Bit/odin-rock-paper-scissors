5261e7c69e7eefb6053fa3168d783002d9e3000a
Lazy-D-Bit <161049991+Lazy-D-Bit@users.noreply.github.com>
2024-04-11 11:35:33 +0530

    Fully Functional js With 5 Rounds of Game

--- script.js
+++ script.js
@@ -14,54 +14,88 @@ function getComputerChoice() {
         return "Scissor";
     }
 }
-let computerSelection = getComputerChoice();
-console.log(`Computer chooses: ${computerSelection}`);
+// let computerSelection = getComputerChoice();
+// console.log(`Computer chooses: ${computerSelection}`);
 function validInput() {
     let playerChoice = prompt("Enter your choice (rock, paper or scissor):");
     console.log(`You wrote: ${playerChoice}`);
     playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.substring(1).toLowerCase();
     while (playerChoice != "Rock" && playerChoice != "Paper" && playerChoice != "Scissor") {
+        console.log("Enter again a valid input");
         playerChoice = prompt("Enter your choice again between these three \"rock, paper or scissor\":")
         console.log(`You wrote: ${playerChoice}`);
         playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.substring(1).toLowerCase();
     }
     return playerChoice;
 }
-playerSelection = validInput();
-console.log(`You choose: ${playerSelection}`);
+// playerSelection = validInput();
+// console.log(`You choose: ${playerSelection}`);
 function playRound(computerSelection, playerSelection) {
     if (computerSelection == "Rock") {
         if (playerSelection == "Paper") {
             console.log("You Won! Paper beats Rock");
+            return 1;
         }
         else if (playerSelection == "Scissor") {
             console.log("You Lose! Rock beats Scissor");
+            return -1;
         }
         else {
             console.log("It's a Draw!");
+            return 0;
         }
     }
     else if (computerSelection == "Paper") {
         if (playerSelection == "Scissor") {
             console.log("You Won! Scissor beats Paper");
+            return 1;
         }
         else if (playerSelection == "Rock") {
             console.log("You Lose! Paper beats Rock");
+            return -1;
         }
         else {
             console.log("It's a Draw!");
+            return 0;
         }
     }
     else if (computerSelection == "Scissor") {
         if (playerSelection == "Rock") {
             console.log("You Won! Rock beats Scissor");
+            return 1;
         }
         else if (playerSelection == "Paper") {
             console.log("You Lose! Scissor beats Paper");
+            return -1;
         }
         else {
             console.log("It's a Draw!");
+            return 0;
         }
     }
 }
-playRound(computerSelection,playerSelection);
+// playRound(computerSelection,playerSelection);
+function playGame() {
+    let score = 0
+    for (let i = 0; i < 5; i++) {
+        console.log(`**** Round ${i+1} ****`);
+        playerSelection = validInput();
+        console.log(`You choose: ${playerSelection}`);
+        let computerSelection = getComputerChoice();
+        console.log(`Computer chooses: ${computerSelection}`);
+        score += playRound(computerSelection,playerSelection);
+    }
+    if (score > 0) {
+        console.log(`You won by ${score} score!!!`);
+        alert(`You won by ${score} score!!!`);
+    }
+    else if (score < 0) {
+        console.log(`You lose by ${score*-1} score!!!`);
+        alert(`You lost by ${score*-1} score!!!`);
+    }
+    else {
+        console.log("Draw!!!");
+        alert(`Draw!!!`);
+    }
+}
+playGame()
